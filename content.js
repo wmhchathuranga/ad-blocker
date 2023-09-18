4903868ddecc00c9836fa2e62a41c8771351ce9b
wmhchathuranga <wmhchathuranga@gmail.com>
2023-09-18 09:45:09 +0530

    Stage 1 done

--- content.js
+++ content.js
@@ -1,8 +1,15 @@
 var newDiv1 = document.createElement('div');
-newDiv1.className = "timebucks-container"
+newDiv1.className = "timebucks-container";
+newDiv1.id = "timebucks-container";
 document.body.appendChild(newDiv1);
 
 var newDiv2 = document.createElement('div');
 newDiv2.textContent = 'Injected Div';
 newDiv2.className = 'content';
 newDiv1.appendChild(newDiv2);
+
+newDiv1.style.display = "none";
+
+if (localStorage.getItem('timebucks-bar') == 1) {
+    newDiv1.style.display = "flex";
+}
--- popup.js
+++ popup.js
@@ -1,18 +1,47 @@
-document.getElementById('injectButton').addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs.length > 0) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                function: injectDiv
-            });
-        } else {
-            console.error("No active tabs found.");
-        }
-    });
+document.getElementById('enable3').addEventListener('click', function () {
+    var enable3Input = document.querySelector('#enable3');
+    if (enable3Input && enable3Input.checked) {
+        localStorage.setItem('timebucks-bar', 1);
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs.length > 0) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: showTimebucksBar
+                });
+            } else {
+                console.error("No active tabs found.");
+            }
+        });
+    }
+    else {
+        localStorage.setItem('timebucks-bar', 0);
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs.length > 0) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: hideTimebucksBar
+                });
+            } else {
+                console.error("No active tabs found.");
+            }
+        });
+    }
 });
 
-function injectDiv() {
-    // var newDiv = document.createElement('div');
-    // newDiv.textContent = 'Injected Div!';
-    // document.body.appendChild(newDiv);
+if (localStorage.getItem('timebucks-bar') == 1) {
+    document.querySelector('#enable3').setAttribute('checked', 'true');
+}
+
+function showTimebucksBar() {
+    localStorage.setItem('timebucks-bar', 1);
+    let timbucks_bar = document.getElementById('timebucks-container');
+    if (timbucks_bar)
+        timbucks_bar.style.display = 'flex';
+}
+
+function hideTimebucksBar() {
+    localStorage.setItem('timebucks-bar', 0);
+    let timbucks_bar = document.getElementById('timebucks-container');
+    if (timbucks_bar)
+        timbucks_bar.style.display = 'none';
 }
